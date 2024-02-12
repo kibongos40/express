f56a0eee138e3054b9c7b29a0a952154b6c8ffd8
kibongos40 <kibongos40@gmail.com>
2024-02-12 18:40:55 +0200

    Mongodb integration and blogs endpoints

--- app.js
+++ app.js
@@ -1,22 +1,62 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+
+const Blog = require("./models/blogModels");
+
 const port = 100;
 
 app.use(express.static("public"));
+app.use(express.json());
 
-// Default
+// Default;
+
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "Welcome to Kibongo Simon Peter's Blog"
+    });
+})
+
+// Get all blogs
+
+app.get("/blogs", async (req, res) => {
+    try {
+        let blogs = await Blog.find({}, "_id title");
+        res.status(200).json(blogs);
+    } catch (error) {
+    }
+})
+
+// New blog
+
+app.post("/blogs/add", async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body);
+        res.status(200).json(blog);
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json(error.message);
+    }
+})
+
+app.all("*", (req, res) => {
+    res.status(404).json({
+        message: `Endpoint ${req.url} was not found!`
+    });
+})
+
+
+// Last Line
 console.clear();
-
-app.get("/", (req,res)=>{
-    res.send("<h1>Murife Don't run, you've arrived</h1>");
-})
-
-app.get("*", (req,res)=>{
-    res.send("<h1>Error 404: Page not found</h1>");
-})
-
-
-app.listen(port, ()=>{
-    console.log(`Running on ${port}`);
-});
+mongoose
+    .connect("mongodb://127.0.0.1:27017/portfolio")
+    .then(() => {
+        console.log("Connected");
+        app.listen(port, () => {
+            console.log(`Running on ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
