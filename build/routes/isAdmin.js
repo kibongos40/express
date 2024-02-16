8b1428c42a00a27f3f80e2fc7cc183f36ad052ee
kibongos40 <kibongos40@gmail.com>
2024-02-16 17:37:51 +0200

    Adding the build folder

---
+++ build/routes/isAdmin.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+function isAdmin(req, res, next) {
+    if (req.headers.authorization) {
+        let token = req.headers.authorization.split(" ")[1];
+        if (token == process.env.ADMIN)
+            next();
+        else
+            res.status(401).json({
+                error: "Invalid token"
+            });
+    }
+    else {
+        console.log("Unauthorised");
+        res.status(401).json({ error: "Unauthorised" });
+    }
+}
+exports.default = isAdmin;
